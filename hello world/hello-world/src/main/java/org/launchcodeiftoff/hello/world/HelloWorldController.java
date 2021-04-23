9d33211a297be66e4877dc689e37c62fa85101f9
Demetrius Bright <demetriusmbright@gmail.com>
2021-04-23 11:32:37 -0500

    created database

---
+++ hello world/hello-world/src/main/java/org/launchcodeiftoff/hello/world/HelloWorldController.java
@@ -0,0 +1,12 @@
+package org.launchcodeiftoff.hello.world;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping(value = "/", method = RequestMethod.GET) public String hello() {
+        return "Hello World!";
+    }
+}
